65a0169528e71c79dc8772e7a1786d7852698899
Elian Silva <you@example.com>
2023-09-19 09:23:24 -0300

    update category

--- src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,8 +4,8 @@ import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,9 +30,23 @@ public class CategoryService {
         return new CategoryDTO(entity);
     }
 
+    @Transactional
     public CategoryDTO insert(CategoryDTO dto) {
         Category entity = new Category(dto.id(), dto.name());
         entity = repository.save(entity);
         return new CategoryDTO(entity);
+    }
+
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO dto) {
+        try {
+            Category entity = repository.getReferenceById(id);
+            entity.setName(dto.name());
+            entity = repository.save(entity);
+            return new CategoryDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Resource not found");
+        }
+
     }
 }
